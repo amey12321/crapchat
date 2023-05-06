8888458aa096b7a96de3c842429f7f7ed3acfe2a
amey12321 <127582238+amey12321@users.noreply.github.com>
2023-05-06 08:44:32 -0700

    Add files via upload

---
+++ crapchat.js
@@ -0,0 +1,6 @@
+function addUser(){
+    document.getElementById("user_name").value;
+    localStorage.getItem("user_name");
+    window.location.replace("crapchat_r.html");
+    user_name = "crapchat_r.html";
+}
---
+++ crapchat_r.js
@@ -0,0 +1,15 @@
+function logout(){
+    window.location.replace("index.html")
+}
+document.getElementById("user_name").innerHTML;
+function addRoom(){
+    firebaseConfig = {
+        apiKey: "AIzaSyAMmTJRd1QyOihlREdKLbQ6HvJqK_SLgKA",
+        authDomain: "crapchat-be80b.firebaseapp.com",
+        databaseURL: "https://crapchat-be80b-default-rtdb.firebaseio.com",
+        projectId: "crapchat-be80b",
+        storageBucket: "crapchat-be80b.appspot.com",
+        messagingSenderId: "630361954609",
+        appId: "1:630361954609:web:f522a289e28e7af8b0f810"
+      };
+}
